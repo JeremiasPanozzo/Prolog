fe1cd89b6233520ddbb9b786fb72056b213028a8
Jeremias Panozzo <jeremiaspanozzo@gmail.com>
2024-08-31 19:17:23 +0000

    Se crea los hechos con familia

---
+++ family.pl
@@ -0,0 +1,38 @@
+progenitor(silvia, silvina).
+progenitor(silvia, marta).
+progenitor(jose, silvina).
+progenitor(jose, marta).
+progenitor(juan, pedro).
+progenitor(juan, luis).
+progenitor(maria, pedro).
+progenitor(maria, luis).
+progenitor(elisa, alicia).
+progenitor(elisa, carlos).
+progenitor(jorge, alicia).
+progenitor(jorge, carlos).
+progenitor(marta, paola).
+progenitor(marta, mario).
+progenitor(pedro, paola).
+progenitor(pedro, mario).
+progenitor(luis, pamela).
+progenitor(luis, pablo).
+progenitor(alicia, pamela).
+progenitor(alicia, pablo).
+
+mujer(silvia).
+mujer(maria).
+mujer(elisa).
+mujer(silvina).
+mujer(marta).
+mujer(alicia).
+mujer(paola).
+mujer(pamela).
+
+varon(jose).
+varon(juan).
+varon(jorge).
+varon(pedro).
+varon(luis).
+varon(carlos).
+varon(mario).
+varon(pablo).
