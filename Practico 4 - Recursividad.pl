4338b4a619f69f53f72632acd4fcde108960b462
Jeremias Panozzo <jeremiaspanozzo@gmail.com>
2024-09-09 22:04:25 -0300

    Se agrega suma_digitos(X, Y).

--- Practico 4 - Recursividad.pl
+++ Practico 4 - Recursividad.pl
@@ -28,5 +28,24 @@ potencia(X, Y, Z):-
     potencia(X, Y1, Z1),
     Z is X * Z1.
     
+%Escribir el predicado fibonacci(N, F), tal que F sea el valor enésimo en la serie de Fibonacci
+fibonacci(0, 0).
+fibonacci(1, 1).
 
+fibonacci(N, X):-
+  N > 1,
+  N1 is N - 1,
+  N2 is N - 2,
+  fibonacci(N1, F1),
+  fibonacci(N2, F2),
+  X is F1 + F2.
+  
+%suma_digitos(X, Y), donde X es un número e Y es el resultado de sumar sus dígitos.
+suma_digitos(0, 0).
 
+suma_digitos(X, Y) :-
+    X > 0,
+    Ultimo_digito is X mod 10,
+    Resto is X // 10,
+    suma_digitos(Resto, SumaResto),
+    Y is UltimoDigito + SumaResto.
